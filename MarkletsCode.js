679ae3d5ca3f01749ea1e8bb5e182f2cb662fa7e
CkR-0ff <gregoslp@gmail.com>
2015-07-02 17:27:23 +0300

    Numer04

--- MarkletsCode.js
+++ MarkletsCode.js
@@ -1,5 +1,31 @@
 javascript: (
   function (){
+    var exists = false;
+    var allScripts = document.getElementsByTagName('script');
+    alert(allScripts.join('\n'));
+    if(allScripts){
+      for(var i = 0; i<allScripts.length; i++){
+	if(allScripts[i].src = 'https://github.com/CkR-0ff/JSRepo/blob/master/btnAdder.js'){
+	  exists = true;
+	}
+      }
+    }
+    
+    if(!exists){
+      var done = false;
+      var scr = document.createElement('script');
+      scr.src = 'https://github.com/CkR-0ff/JSRepo/blob/master/btnAdder.js';
+      scr.onload = scr.onreadystatechange = function(){
+	  if (!done && (!this.readyState || this.readyState == "loaded" || this.readyState == "complete")) {
+	      done = true;
+	      alert('ScriptLoaded');
+	      btnCreator();
+	  }
+      };
+    }else{
+      alert('ScriptAlreadyThere');
+      btnCreator();
+    }
     
   }();
 );
