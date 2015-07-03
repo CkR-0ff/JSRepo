c174b5f79a9dd7d2ffd195031558a38d558bfe94
CkR-0ff <gregoslp@gmail.com>
2015-07-03 11:23:32 +0300

    22334

--- btnAdder.js
+++ btnAdder.js
@@ -1,13 +1,6 @@
 var btnAdderScript = function (){
-  var btn = btnCreator();
-  btn.click( function (){
-    btnAdderScript();
-  });
-  
-}
-
-var btnCreator = function () {
   var btn = document.createElement('button');
   document.getElementsByTagName('body')[0].appendChild(btn);
-  return btn;
+  btn.click(btnAdderScript());
+  
 }
