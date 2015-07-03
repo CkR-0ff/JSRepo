48f8827fb857b0b76a4206a0170b6bff9d297fcf
CkR-0ff <gregoslp@gmail.com>
2015-07-03 16:46:49 +0300

    repeater

---
+++ NeverStoper.js
@@ -0,0 +1,27 @@
+var roller = function(){
+  var msgs = [];
+  msgs.push('Welll... hello there!!! Can I ask you a favor... click that OK button for me... will ya.');
+  msgs.push('Ohhh that was nice... You are very good at this... Do it again... ');
+  msgs.push('You just can\'t be that good... It\'s not your first time is it... HAHAAAA(you get it... cause you already did it twice');
+  msgs.push('');
+  msgs.push('');
+  msgs.push('');
+  msgs.push('');
+  msgs.push('');
+  msgs.push('');
+  msgs.push('');
+  msgs.push('');
+  msgs.push('');
+  msgs.push('');
+  msgs.push('');
+  msgs.push('');
+  msgs.push('');
+  msgs.push('');
+  msgs.push('');
+  msgs.push('');
+  msgs.push('');
+  
+  for(var i=0;i<msgs.length; i++){
+    alert(msgs[i]);
+  }
+}
