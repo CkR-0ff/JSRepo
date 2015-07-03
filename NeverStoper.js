3786112f8849545b3502d2b6a887beae5d7ad6ad
CkR-0ff <gregoslp@gmail.com>
2015-07-03 17:38:36 +0300

    snaaaail

--- NeverStoper.js
+++ NeverStoper.js
@@ -7,8 +7,8 @@ var roller = function(){
   msgs.push('Stop it you crazy bastard... the whole world(or your browser) may explode...');
   msgs.push('Are you scared that this may never stop?');
   msgs.push('I mean seroisly... check that little checkbox enytime... and just skip all this nosense...');
-  msgs.push('That\'s my boy... never giving up... keep on clicking...');
-  msgs.push('Not very susceptible to revers psychologie uh?');
+  msgs.push('That\'s my boy... never giving up... Go on then... keep on clicking...');
+  msgs.push('Not very susceptible to revers psychology uh?');
   msgs.push('That it hurts me... it hurts so much... PLEASE STOP... PLEAAAAS... ');
   msgs.push('Ok... that\'s not working eather... How much do you want...');
   msgs.push('Seriously name a price... And give me your bank account... ');
@@ -18,10 +18,10 @@ var roller = function(){
   msgs.push('FOR YOU');
   msgs.push('OK... I give up... push it all you want... ');
   msgs.push('BTW... every time you click OK... I kill a baby snail... ');
-  msgs.push('Seriously... I electricute their slimy asses...');
-  msgs.push('You seriously have no love for snails then... "You hear thet snails... he hates you..."');
+  msgs.push('Seriously... I electrocute their slimy asses...(HAH ElectroCute... it\'s lika a female DJ or something)');
+  msgs.push('You seriously have no love for snails then... "You hear that snails... he hates you..."');
   msgs.push('They are angry now... expect an angry snail mob soon.. they gonna slime yo ass...');
-  msgs.push('Oooops another one... electricuted... BECAUSE OF YOU...');
+  msgs.push('Oooops another one... electrocuted... BECAUSE OF YOU...');
   msgs.push('Fuck... where did that one come from... ');
   msgs.push('Holy fuck... Holy fuck... Snails on the loose... snails on the loose... ');
   msgs.push('Interrupting transmission because of angry snail mob problems... congrats... I\'m dead... Hope You\'re happy now...');
