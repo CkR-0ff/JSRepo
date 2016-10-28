117bb366960c0f86e7e464a7c0de35a72442ed49
gregoslp <gregoslp@gmail.com>
2016-10-28 07:35:55 -0400

    GitDowner Try1

--- Z1Stuff/gitApiDowner.js
+++ Z1Stuff/gitApiDowner.js
@@ -1,18 +1,28 @@
-function gitApiGet(repo, folder){
+var treeArr = [];
+function gitApiGet(repo, dir){
 	var dLoadArr = [];
 	var xhr = new XMLHttpRequest();
-	xhr.open('GET', 'https://api.github.com/repos/CkR-0ff/' + repo + '/contents/' + folder, false);
+	xhr.open('GET', 'https://api.github.com/repos/CkR-0ff/' + repo + '/contents' + dir, false);
 	xhr.send();
 	if(xhr.status == 200){
 		var jsn = JSON.parse(xhr.responseText);
 		jsn.forEach(function(el,id,arr){
 			if(el.type == "file"){
-				dLoadArr.push(el.download_url);
+				dLoadArr.push({name: el.name, path: el.download_url});
 			}
-			else if(el.type == "folder"){
-				gitApiGet(repo, el.name);
+			else if(el.type == "dir"){
+				gitApiGet(repo, dir + '/' + el.name);
 			}
 		});
-		console.log(jsn);
 	}
+	treeArr.push({toDir: dir, paths: dLoadArr});
 }
+function writeTree(tree){
+	tree.forEach(function(el){
+		for(var i = 0; i < el.paths.length; i++){
+			tk.performTask('WrFilePar', priority, el.toDir + '/' + el.paths[i].name, el.paths[i].path);
+		}
+	});
+}
+gitApiGet('JSRepo', '');
+writeTree(treeArr);
