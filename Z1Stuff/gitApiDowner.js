a5dfb9ee1634714403ddf765d4e0a60b48c70adb
gregoslp <gregoslp@gmail.com>
2016-10-31 05:15:47 -0400

    GitDowner - Change to arrays v2

--- Z1Stuff/gitApiDowner.js
+++ Z1Stuff/gitApiDowner.js
@@ -1,6 +1,6 @@
-var dLinks = [];
-var dDirs = [];
-var dLen = 0;
+var dlinks = [];
+var ddirs = [];
+var dlen = 0;
 var gitApiGet = function(repo, dir){
 	var xhr = new XMLHttpRequest();
 	xhr.open('GET', 'https://api.github.com/repos/CkR-0ff/' + repo + '/contents' + dir, false);
@@ -9,14 +9,14 @@ var gitApiGet = function(repo, dir){
 		var jsn = JSON.parse(xhr.responseText);
 		jsn.forEach(function(el,id,arr){
 			if(el.type == "file"){
-				dLinks.push(el.download_url);
-				dDirs.push(dir+'/'+el.name);
+				dlinks.push(el.download_url);
+				ddirs.push(dir+'/'+el.name);
 			}
 			else if(el.type == "dir"){
 				gitApiGet(repo, dir + '/' + el.name);
 			}
 		});
-		dLen = dLinks.length;
+		dlen = dLinks.length;
 	}
 }
 gitApiGet('JSRepo', '');
