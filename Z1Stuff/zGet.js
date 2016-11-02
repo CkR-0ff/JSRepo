5a632cda88937a4da1958cee08ee35f7cb9aa9bc
gregoslp <gregoslp@gmail.com>
2016-11-02 09:10:45 -0400

    zGet - mofo link

--- Z1Stuff/zGet.js
+++ Z1Stuff/zGet.js
@@ -30,7 +30,7 @@ var parseTip = function(msg){
   tempCont.innerHTML = msg.substr(start+1, end-start-1);
   var imgEl = tempCont.getElementsByTagName('img')[0];
   var link = imgEl.src.substr(imgEl.src.indexOf('http')+7);
-  link.slice(0, -1);
+  link.slice(0, -2);
   return link;
 };
 
