779068713865106c6846eeedfe78a00685abaa81
gregoslp <gregoslp@gmail.com>
2016-11-08 08:28:04 -0500

    Widget xhr get img with blob

--- Z1Stuff/XHRImg.js
+++ Z1Stuff/XHRImg.js
@@ -2,11 +2,10 @@ function XHRImgLoad(path){
   var img;
   var xhr = new XMLHttpRequest();
   xhr.open("GET", path, false);
-  xhr.responseType('blob');
   xhr.send();
   flash(xhr.status);
   if (xhr.status == 200) {
     img = xhr.response;
   }
-  return URL.createObjectURL(img);
+  return img;
 }
