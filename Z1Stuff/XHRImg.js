cee17ccc53718a2e292830ba41f406ec32247edd
gregoslp <gregoslp@gmail.com>
2016-11-08 07:20:27 -0500

    Widget xhr get img

--- Z1Stuff/XHRImg.js
+++ Z1Stuff/XHRImg.js
@@ -1,11 +1,11 @@
 function XHRImgLoad(path){
   var img;
   var xhr = new XMLHttpRequest();
-  xhr.responseType('image/*');
+  xhr.responseType('blob');
   xhr.open("GET", path, false);
   xhr.send();
   if (xhr.status == 200) {
     img = xhr.response;
   }
-  return img;
+  return URL.createObjectURL(img);
 }
