469bb94542f5f9b0f480d51888891dfe32c72bec
gregoslp <gregoslp@gmail.com>
2016-11-15 06:17:31 -0500

    LL try auto 2

--- Z1Stuff/LLScripts/GetIntent.js
+++ Z1Stuff/LLScripts/GetIntent.js
@@ -1,4 +1,4 @@
 var dsc = LL.getDesktopByName('MainDesk');
 var wg = dsc.getItemByName('Wgt');
-wg.setLabel(wg.getIntent().toUri(0), true);
+wg.setLabel(wg.getIntent().toUri(1), true);
 //LL.save();
