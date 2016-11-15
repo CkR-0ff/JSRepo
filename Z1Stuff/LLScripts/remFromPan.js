0ee64d86fe933632b263e99a84b933678c890bbe
gregoslp <gregoslp@gmail.com>
2016-11-15 05:57:24 -0500

    New Tree with LL

--- Z1Stuff/LLScripts/remFromPan.js
+++
@@ -1,7 +0,0 @@
-var dsc = LL.getCurrentDesktop();
-var pan = dsc.getItemByName("thepan");
-var pancon = pan.getContainer();
-var itms = pancon.getItems();
-for (var i = 0; i < itms.length; i++) {
-  pancon.removeItem(itms.getAt(i));
-}
