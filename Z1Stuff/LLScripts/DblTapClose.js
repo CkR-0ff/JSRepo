f33915d7b9916e8908f742f7e371481cdf3af002
gregoslp <gregoslp@gmail.com>
2016-11-15 08:46:03 -0500

    LL Zami Hide PopUp

--- Z1Stuff/LLScripts/DblTapClose.js
+++ Z1Stuff/LLScripts/DblTapClose.js
@@ -1,3 +1,3 @@
-var callerItem = LL.getEvent().getItem();
+var callerItem = LL.getEvent().getContainer();
 alert(callerItem);
 callerItem.getParent().removeItem(callerItem.getParent().getItemByName('ZamiPopUp'));
