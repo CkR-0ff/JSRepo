bc1c91bd9a663313bd3219eed74308a5fa3efe2e
gregoslp <gregoslp@gmail.com>
2016-12-02 08:26:03 -0500

    Widget trr down

--- Z1Stuff/LLScripts/TorrDown.js
+++ Z1Stuff/LLScripts/TorrDown.js
@@ -1,5 +1,7 @@
+var sender = LL.getEvent().getItem();
+var data = JSON.parse(sender.getTag('inData'));
 if ( TaskerIntent.testStatus( this ).equals( TaskerIntent.Status.OK ) ) { 
 	TaskerIntent intn = new TaskerIntent( "GetTorrent" );
-	intn.addVariable( "%name", "value" );
+	intn.addVariable( "%torlink", "value" );
 	sendBroadcast( intn );
 }
